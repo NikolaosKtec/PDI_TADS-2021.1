0752bd3717b642721e8e1125619e411094eae05d
nikolaos ferreira <nikolaosf1daskalakis@gmail.com>
2022-08-26 22:20:24 -0300

    updates

--- togray.m
+++ togray.m
@@ -11,5 +11,17 @@ figure('Name','img');
    im(:,:,3) = im(:,:,2);
 imshow(im)
 toc
+%efeito degradê
+tic
+figure('Name','image 2');
+im2 = im;
+for i = 1:size(im2,2)
+  im2(:,i,1) -= ((155)-i*0.5);
+  im2(:,i,2) -= ((155)-i*0.5);
+  im2(:,i,3) -= ((155)-i*0.5);
+endfor
+
+imshow(im2)
+toc
 
 
